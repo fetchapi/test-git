c3f0e1d6cc30b4a2042ec6e41d46a7148c8386fb
khacdat328 <datchau.12111994@gmail.com>
2023-08-08 09:13:18 +0700

    Change todoModule

--- src/store/todoModule.js
+++ src/store/todoModule.js
@@ -3,8 +3,7 @@ const todoModule = {
     state: {
         list: [
             { id: 1, title: 'Viec 1', completed: false },
-            { id: 2, title: 'Viec 2', completed: false },
-            { id: 3, title: 'Viec 3', completed: false },
+            { id: 3, title: 'Viec 3', completed: true },
         ]
     },
     mutations: {
